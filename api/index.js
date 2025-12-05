2583e00ae3c10e51d05020605992c90cbf47410d
Ashikur Rahman <ashikurahmantuhin@gmail.com>
2025-12-05 20:26:45 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -48,6 +48,6 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.listen(port, () => {
-  // console.log(`Course Master is Running on port ${port}`);
- // });
+app.listen(port, () => {
+  console.log(`Course Master is Running on port ${port}`);
+  });
